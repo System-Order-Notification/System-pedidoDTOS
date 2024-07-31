6e89fc516936d623249d24ceb354cb417639f347
AndrewAscar742 <andrewmatosascar@outlook.com>
2024-07-31 15:20:01 -0300

    build: Initial Commit

---
+++ src/main/java/br/com/sp/pedidoDTOS/PedidoDtosApplication.java
@@ -0,0 +1,13 @@
+package br.com.sp.pedidoDTOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoDtosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoDtosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sp/pedidoDTOS/PedidoDtosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sp.pedidoDTOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoDtosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
